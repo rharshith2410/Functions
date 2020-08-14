a496cedbcdd2054f64a43713ef32f960b19fb297
rharshith2410 <68144219+rharshith2410@users.noreply.github.com>
2020-08-14 15:34:35 +0530

    Create Func.py

---
+++ Func.py
@@ -0,0 +1,10 @@
+def most_frequent(string):
+    d=dict()
+    for key in string:
+        if key not in d:
+            d[key]=1
+        else:
+            d[key]+=1
+    sorted_orders = sorted(d.items(),key=lambda x:x[1],reverse=True)
+
+most_frequent("Mississippi")
